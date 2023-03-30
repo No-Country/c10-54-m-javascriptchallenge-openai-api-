5d024951f761ceb2d056296def05daf39b8075ca
camunozn <camunozn89@gmail.com>
2023-03-30 12:21:42 -0500

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const db = require("./utils/database");
+
+const app = express();
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+const PORT = 8000;
+
+db.authenticate()
+  .then(() => {
+    console.log("DB connection successful");
+  })
+  .catch((error) => console.log(error));
+
+db.sync()
+  .then(() => console.log("DB synchronized"))
+  .catch((error) => console.log(error));
+
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/orders", orderRoutes);
+
+app.listen(PORT, () => {
+  console.log(`App running on port: ${PORT}`);
+});
---
+++ src/utils/database.js
@@ -0,0 +1,16 @@
+const { Sequelize } = require("sequelize");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config.env" });
+
+const db = new Sequelize({
+  database: process.env.DATABASE,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  dialect: "postgres",
+  logging: false,
+});
+
+module.exports = db;
